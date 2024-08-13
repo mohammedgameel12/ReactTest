39e6c4c715832c6caedb11de583bfe35a3935ef5
gimmy <gimmy@gmail.com>
2024-08-13 22:52:10 +0300

    Create counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Hello World!</h1>
+      <Counter />
     </div>
   );
 }
